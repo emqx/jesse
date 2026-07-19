dc791aa809fdc64bd2aa277abcb248824970076c
zmstone <zmstone@gmail.com>
2026-07-19 18:19:34 +0200

    chore: set EMQ copyright on new draft 2019-09/2020-12 modules

--- test/jesse_tests_draft2019_09_SUITE.erl
+++ test/jesse_tests_draft2019_09_SUITE.erl
@@ -1,5 +1,5 @@
 %%%=============================================================================
-%% Copyright 2024- AUTHORS
+%% Copyright (c) 2026 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
